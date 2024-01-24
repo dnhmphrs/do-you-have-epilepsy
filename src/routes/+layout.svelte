049fff9d6d955e4833788150fdcc860006ad154d
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-01-24 18:19:22 +0000

    make dumb site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,19 +27,50 @@
 		// device type
 		screenType.set(getDeviceType());
 		isIframe.set(window.location !== window.parent.location);
-	} 
+	}
 
+	let showShader = false;
+	let showEpilepsyWarning = true;
+	let showCompletionMessage = false;
+	const epilepsyTestDuration = 5000; // 5 seconds
+
+	const handleKeyPress = (event) => {
+		if (event.key === ' ') {
+			if (showEpilepsyWarning) {
+				startEpilepsyTest();
+			} else if (showCompletionMessage) {
+				repeatEpilepsyTest();
+			}
+		}
+	};
+
+	const startEpilepsyTest = () => {
+		showEpilepsyWarning = false;
+		showShader = true;
+		setTimeout(() => {
+			showShader = false;
+			showCompletionMessage = true;
+		}, epilepsyTestDuration);
+	};
+
+	const repeatEpilepsyTest = () => {
+		showCompletionMessage = false;
+		startEpilepsyTest();
+	};
 
 	onMount(async () => {
 		// webgl
 		const module = await import('$lib/graphics/webgl.svelte');
-    Geometry = module.default;
+		Geometry = module.default;
 
 		handleScreen();
 		window.addEventListener('resize', () => handleScreen());
 
+		window.addEventListener('keydown', handleKeyPress);
+
 		return () => {
 			window.removeEventListener('resize', () => handleScreen());
+			window.removeEventListener('keydown', handleKeyPress);
 		};
 	});
 </script>
@@ -51,53 +82,31 @@
 	<meta name="author" content="AUFBAU" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<link
-		rel="preload"
-		href="/aufbau.svg"
-		as="image"
-		type="image/svg+xml"
-		crossorigin="anonymous"
-	/>
-
-	<!-- <link
-		rel="preload"
-		href="/fonts/NB-Architekt-Pro-Light.woff"
-		as="font"
-		type="font/woff"
-		crossorigin="anonymous"
-	/> -->
-
-	<!-- <link
-		rel="preload"
-		href="/fonts/NB-Architekt-Pro-Bold.woff"
-		as="font"
-		type="font/woff"
-		crossorigin="anonymous"
-	/> -->
-
-	<!-- <link
-	rel="preload"
-	href="/fonts/Dahlia-Medium.woff2"
-	as="font"
-	type="font/woff2"
-	crossorigin="anonymous"
-/> -->
-
+	<link rel="preload" href="/aufbau.svg" as="image" type="image/svg+xml" crossorigin="anonymous" />
 </svelte:head>
 
-{#if Geometry}
-    <svelte:component this={Geometry} />
-{:else}
-    <div class="loading">loading.</div>
+{#if showEpilepsyWarning}
+	<div class="epilepsy-warning">
+		<i>www.do-you-have-epilepsy.com</i>
+		<br />
+		<br />
+		Press <b>spacebar</b> to start epilepsy test
+		<br />
+		<br />
+		<b class="important">VERY IMPORTANT DISCLAIMER:<br />DO NOT USE IF YOU ACTUALLY HAVE EPILEPSY</b
+		>
+	</div>
+{:else if showShader}
+	<svelte:component this={Geometry} />
+{:else if showCompletionMessage}
+	<div class="completion-message">
+		Congratulations.
+		<br /><br />
+		<b>If you made it here,<br />you do not have epilepsy.</b>
+		<br /><br />
+		Press <b>spacebar</b> to repeat the test
+	</div>
 {/if}
-
-<div class="app">
-
-	<main>
-		<slot />
-	</main>
-
-</div>
 
 <style>
 	.app {
@@ -109,6 +118,7 @@
 		max-height: 100vh;
 		width: 100%;
 		overflow: hidden;
+		background: black;
 	}
 
 	.loading {
@@ -122,6 +132,39 @@
 		font-size: 12px;
 	}
 
+	.epilepsy-warning,
+	.completion-message {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		text-align: center;
+		font-size: 16px;
+		padding: 20px;
+		background-color: white;
+		border-radius: 10px;
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+		color: black;
+	}
+
+	b {
+		font-weight: bold;
+		color: black;
+	}
+
+	i {
+		font-style: italic;
+		font-weight: bold;
+		color: black;
+		text-decoration: underline 1px rgba(0, 0, 0, 1);
+		text-underline-offset: 4px;
+	}
+
+	b.important {
+		text-decoration: underline 1px rgba(0, 0, 0, 1);
+		text-underline-offset: 4px;
+	}
+
 	main {
 		flex: 1;
 		display: flex;
@@ -132,5 +175,4 @@
 		width: 100%;
 		height: 100%;
 	}
-
 </style>
